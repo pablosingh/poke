7a06ab832a11d2a00c3ed61cda3c93e654be0ead
Singh Pablo <pablo22_4@hotmail.com>
2022-09-20 22:33:25 -0300

    lazo giratorio en loading

--- src/components/Loading.jsx
+++ src/components/Loading.jsx
@@ -1,12 +1,23 @@
-// import imgLoading from '../img/trueno_pikachu.gif';
 import imgLoading from '../img/charizard_black.gif';
 import s from '../styles/Loading.module.css';
+import { AiOutlineReload } from 'react-icons/ai';
+import styled from 'styled-components';
 
 export const Loading = () => {
     return (
         <div className={s.container}>
             <img src={imgLoading} alt="loading" className={s.img}/>
-            <h1 className={s.h1}>Cargando...</h1>
+            <Title>
+                <h1 className={s.h1}>Cargando </h1>
+                <AiOutlineReload className={s.logo} />
+            </Title>
         </div>
     )
 };
+
+const Title = styled.div`
+    width: 100%; 
+    display: flex;
+    align-items: center;
+    justify-content: center;
+`;
